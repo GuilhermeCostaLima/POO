23f9cc5f3d4d01f8f8f453214136db9e7b74b7fb
Guilherme da Costa de Lima <127263348+GuilhermeCostaLima@users.noreply.github.com>
2024-10-02 20:09:18 -0300

    Bmol

---
+++ Bmol/aqui/Bmol/BmolApplication.java
@@ -0,0 +1,14 @@
+package br.univille.Bmol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmolApplication.class, args);
+	}
+
+}
+
