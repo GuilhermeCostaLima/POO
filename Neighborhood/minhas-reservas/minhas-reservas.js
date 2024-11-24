b711a95339378c51391243e3554e91a2cd638a84
Guilherme da Costa de Lima <127263348+GuilhermeCostaLima@users.noreply.github.com>
2024-11-24 17:06:22 -0300

    hotel

--- Neighborhood/minhas-reservas/minhas-reservas.js
+++ Neighborhood/minhas-reservas/minhas-reservas.js
@@ -1,6 +1,4 @@
-// Função para alternar a exibição das informações adicionais
 function toggleInfo(cardNumber) {
     const additionalInfo = document.getElementById('additionalInfo' + cardNumber);
-    // Alterna a visibilidade da seção de mais informações
     additionalInfo.style.display = (additionalInfo.style.display === 'block') ? 'none' : 'block';
 }
