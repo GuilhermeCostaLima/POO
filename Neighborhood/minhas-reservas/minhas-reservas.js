292c4e4a5cc129a9d5e07fd79679d49cff42372c
guilherme <guilhermeguilhermev2@gmail.com>
2024-11-25 18:17:29 -0300

    hotel-upg

--- Neighborhood/minhas-reservas/minhas-reservas.js
+++ Neighborhood/minhas-reservas/minhas-reservas.js
@@ -2,3 +2,4 @@ function toggleInfo(cardNumber) {
     const additionalInfo = document.getElementById('additionalInfo' + cardNumber);
     additionalInfo.style.display = (additionalInfo.style.display === 'block') ? 'none' : 'block';
 }
+
