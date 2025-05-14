711f35abc732750ca2da516a0c80f6732aa2f634
Guilherme da Costa de Lima <127263348+GuilhermeCostaLima@users.noreply.github.com>
2025-05-14 19:43:24 -0300

    +-

---
+++ HOTELPOO/src/test/java/br/univille/neighborhood/NeighborhoodFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.neighborhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
